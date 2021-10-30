fe1e7201d67cc35a38022afecf2fc6282dc20a63
Angelika Jungco <angelika.jungco@usjr.edu.ph>
2021-10-30 22:19:38 +0800

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'testproject-35b2e',
+    appId: '1:495529158926:web:3a184fc49536769bde0f5f',
+    storageBucket: 'testproject-35b2e.appspot.com',
+    apiKey: 'AIzaSyCurCC0cvUcewVqNBfpKpZYzYOz7SglBjc',
+    authDomain: 'testproject-35b2e.firebaseapp.com',
+    messagingSenderId: '495529158926',
+    measurementId: 'G-0PV6HF2TJV',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'testproject-35b2e',
+    appId: '1:495529158926:web:3a184fc49536769bde0f5f',
+    storageBucket: 'testproject-35b2e.appspot.com',
+    apiKey: 'AIzaSyCurCC0cvUcewVqNBfpKpZYzYOz7SglBjc',
+    authDomain: 'testproject-35b2e.firebaseapp.com',
+    messagingSenderId: '495529158926',
+    measurementId: 'G-0PV6HF2TJV',
+  },
   production: false
 };
 
